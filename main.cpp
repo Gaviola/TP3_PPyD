81a3d8009a2cf8be1b0f80852048724907e91389
Yeu07 <yeumensilva9@gmail.com>
2024-04-19 12:38:34 -0300

    ejercicio 7

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 #include <cstdio>
 #include <mpi.h>
 #include <math.h>
+#include <cstring>
 
 using std::cout, std::endl;
 using std::string;
 using std::fixed;
+using std::strcpy;
 
 void ejercicio3();
 void ejercicio4();
+void ejercicio7();
 
 int main() {
     //ejercicio3();
-    ejercicio4();
+    //ejercicio4();
+    ejercicio7();
 }
 
 void ejercicio3() {
@@ -43,5 +47,38 @@ void ejercicio4() {
         double tiempo = MPI_Wtime() - tiempo_inicio;
         cout << fixed << tiempo << endl;
     }
+    MPI_Finalize();
+}
+
+void Bcast(int  source, char *msg, int tam){
+
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if( rank == source){
+        for(int i = 0; i < size; i++){
+            MPI_Send(msg, tam, MPI_CHAR, i, 0, comm);
+        }
+    }
+    else{
+        MPI_Recv(msg, tam, MPI_CHAR, source, 0, comm, MPI_STATUS_IGNORE);
+    }
+}
+
+void ejercicio7(){
+    MPI_Init(nullptr, nullptr);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const int tam = 50;
+    char mensaje[tam];
+    if (rank == 0){
+        strcpy(mensaje, "Hello World");
+    }
+
+    Bcast(0,mensaje,tam);
+
+    cout << "Proceso " << rank << " tiene el mensaje: " << mensaje << endl;
+
     MPI_Finalize();
 }
